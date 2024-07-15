698f4a44a0aaa3d20072fb237866fcd86bdb9020
armatura24 <armaturapengva2222@gmail.com>
2024-07-15 21:56:09 +0300

    updated hentai_lib to use the new api

--- manga/hentai_lib/gallery.js
+++ manga/hentai_lib/gallery.js
@@ -1,33 +1,13 @@
 let fun = async () => {
-    const canvas = document.createElement("canvas");
-    const body = document.querySelector("body");
-    body.appendChild(canvas);
-
-    const ctx = canvas.getContext("2d");
-    const res = await Promise.all(Array.from(document.querySelectorAll('.media-card')).map(card => {
-        const image = document.createElement("img");
-        return new Promise((resolve, reject) => {
-            image.src = "https://hentailib.me" + card.getAttribute("data-src");
-            image.onload = () => {
-
-                canvas.width = image.naturalWidth;
-                canvas.height = image.naturalHeight;
-                ctx.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight);
-                const base64 = canvas.toDataURL("image/jpeg");
-
-                resolve({
-                    uid: card.href.split('/').pop(),
-                    title: card.querySelector(".media-card__title").textContent,
-                    // url: "https://hentailib.me" + card.getAttribute("data-src"),
-                    cover: base64,
-                    data: card.textContent
-                });
-
-            };
-        });
+    return await Promise.all(Array.from(document.querySelectorAll('.card-item')).map(card => {
+        return {
+            uid: card.querySelector("a").href.split('/').pop(),
+            title: card.querySelector(".card-item-caption__main").textContent,
+            // url: "https://hentailib.me" + card.getAttribute("data-src"),
+            cover: card.getAttribute("src"),
+            data: card.textContent
+        };
     }));
-    body.removeChild(canvas);
-    return res;
 }
 
 return await fun();
