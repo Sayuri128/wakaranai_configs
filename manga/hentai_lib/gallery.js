8d559e3b2640be74ef2aba966e203063b3bd0fd0
armatura24 <vivcharenko10@gmail.com>
2024-12-29 15:16:00 +0200

    fix: migrated lib group sources to rest api

--- manga/hentai_lib/gallery.js
+++ manga/hentai_lib/gallery.js
@@ -1,13 +1,22 @@
 let fun = async () => {
-    return await Promise.all(Array.from(document.querySelectorAll('.card-item')).map(card => {
+    const canvas = document.createElement("canvas");
+    const body = document.querySelector("body");
+    body.appendChild(canvas);
+
+    const ctx = canvas.getContext("2d");
+    const res = await Promise.all(Array.from(document.querySelectorAll('.card-item')).map(card => {
+        const image = document.createElement("img");
         return {
-            uid: card.querySelector("a").href.split('/').pop(),
+            uid: card.getAttribute("data-media-slug"),
             title: card.querySelector(".card-item-caption__main").textContent,
-            // url: "https://hentailib.me" + card.getAttribute("data-src"),
-            cover: card.getAttribute("src"),
+            // url: "https://mangalib.me" + card.getAttribute("data-src"),
+            cover: card.querySelector("img").src,
             data: card.textContent
-        };
+        }
     }));
+    console.log(res)
+    body.removeChild(canvas);
+    return res;
 }
 
 return await fun();
